6b66094c6b377ae45e286fd74fbf4a5c3089fe6f
Megan Zehrt <zehrtmegan@gmail.com>
2024-05-11 18:34:48 -0500

    first commit

---
+++ algos.js
@@ -0,0 +1,42 @@
+// 230. Kth Smallest Element in a BST
+
+// Given the root of a binary search tree, and an integer k, return the kth smallest value (1-indexed) of all the values of the nodes in the tree.
+
+
+
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} k
+ * @return {number}
+ */
+var kthSmallest = function(root, k) {
+    
+    let results = []
+
+    function dfs(node, results){
+
+        if(node === null){
+            return null
+        }
+
+        results.push(node.val)
+
+        dfs(node.left, results)
+        dfs(node.right, results)
+    }
+
+    dfs(root, results)
+
+    results.sort((a,b) => a-b)
+
+    return results[k-1]
+};
